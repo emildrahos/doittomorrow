a9d44b55f9721e16ea2a617edccae676c892ab43
Emil Drahos <emildrahos@gmail.com>
2026-03-17 12:41:12 +0100

    Initial Commit

---
+++ doittomorrow/doittomorrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  doittomorrowApp.swift
+//  doittomorrow
+//
+//  Created by Emil Drahoš on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct doittomorrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
